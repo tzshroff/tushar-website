5f6a47702dfa191e76b61944731bd14b5b830773
Tushar Shroff <tzshroff@hotmail.com>
2025-06-12 10:55:12 -0400

    Initial commit with videos and code

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/vision")
+def vision():
+    return render_template("vision.html")
+
+@app.route("/songs")
+def songs():
+    return render_template("songs.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
